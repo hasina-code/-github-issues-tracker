b56a7b51eb24707c8a03330ddfa75fd2f905bdbf
hasina-code <hasina.akter171407@gmail.com>
2026-03-09 10:35:28 +0600

    Add All/Open/Closed tab filter and active tab styling

---
+++ script.js
@@ -0,0 +1,69 @@
+let allIssues = [];
+console.log("allIssues:", allIssues);
+// Show loader
+const showLoader = () => {
+  console.log("Loader Showing...");
+  document.getElementById("loader").style.display = "flex";
+};
+
+const hideLoader = () => {
+  console.log("Loader Hidden");
+  document.getElementById("loader").style.display = "none";
+};
+
+
+
+// Display issues in cards
+const displayIssues = (issues) => {
+  const container = document.getElementById("issues-container");
+  container.innerHTML = "";
+
+  if (!issues || issues.length === 0) {
+    container.innerHTML = `<p class="text-center col-span-full text-gray-500">No issues found.</p>`;
+    return;
+  }
+
+
+};
+
+// Update Issue Count
+const updateIssueCount = (issues) => {
+ // console.log("Issue Count:", issues.length);
+  document.getElementById("issue-count").textContent = `${issues.length} Issues`;
+};
+
+
+// Tabs functionality with loader
+const setupTabs = () => {
+  const tabs = ["all", "open", "closed"];
+  tabs.forEach(tab => {
+    document.getElementById(`tab-${tab}`).addEventListener("click", () => {
+      showLoader();
+      setTimeout(() => {
+        let filtered = [];
+        if(tab === "all") filtered = allIssues;
+        else filtered = allIssues.filter(issue => issue.status === tab);
+        displayIssues(filtered);
+        updateIssueCount(filtered);
+        setActiveTab(`tab-${tab}`);
+        hideLoader();
+      }, 200);
+    });
+  });
+};
+
+// Active Tab Styling
+const setActiveTab = (tabId) => {
+  document.querySelectorAll(".tab-btn").forEach(btn => {
+    btn.classList.remove("btn-primary");
+    btn.classList.add("btn-outline");
+  });
+  const activeBtn = document.getElementById(tabId);
+  activeBtn.classList.add("btn-primary");
+  activeBtn.classList.remove("btn-outline");
+};
+
+
+// Initialize
+setupTabs();
+loadIssues();
